45d5e80e65b4ef40378572c95d0d22bf108e5ea4
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2023-04-19 13:16:47 -0400

    restructuring portfolio to include react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import PageContainer from './PageContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<PageContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PageContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PageContainer() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default PageContainer
